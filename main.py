65b3885b60a7e727676c17b353c8540a5b7973e6
Manvendra Singh <manvendrask2@gmail.com>
2026-05-05 02:00:58 +0530

    Integrate TavilyClient for search functionality and update LLM model to GPT-5

--- main.py
+++ main.py
@@ -3,29 +3,36 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
+from tavily import TavilyClient
 
 load_dotenv()
 
+tavily = TavilyClient()
+
 
 @tool
-def search(query: str) -> str:
+def search(query: str) -> dict:
     """
     Tool that searches over internet
     :param query: The query to search for
     :return: The search result
     """
     print(f"Searching for {query}")
-    return "Tokyo weather is sunny."
+    return tavily.search(query=query)
 
 
-llm = ChatOpenAI()
+llm = ChatOpenAI(model="gpt-5")
 tools = [search]
 agent = create_agent(model=llm, tools=tools)
 
 
 def main():
     result = agent.invoke(
-        input={"messages": HumanMessage(content="What is the weather in Tokyo?")}
+        input={
+            "messages": HumanMessage(
+                content="What is the weather in Faridabad, Haryana?"
+            )
+        }
     )
     print(result)
 
